a6645ae9b74eea5a31b2c7d5104f3ec331e4ca6e
developeeer-level1 <yak.yakovochka@gmail.com>
2025-04-09 23:02:20 +0100

    Add project files.

---
+++ dz13/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace dz13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ну пожалуйста, не возвращайте дзшу(");
+            //Ну пожалуйста, не возвращайте дзшу(
+        }
+    }
+}
